a5495d17f14e7604bb6cff6e0551f4e016e0df2c
rutar <rustarg@gmail.com>
2025-04-13 18:48:52 +0300

    Add base components.

---
+++ frontend/src/app/components/hero/hero.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero',
+  imports: [],
+  template: `
+    <p>
+      hero works!
+    </p>
+  `,
+  styles: ``
+})
+export class HeroComponent {
+
+}
