b51bf3530c0b111ed33a1a184ab000fdef576c54
rutar <rustarg@gmail.com>
2025-04-13 20:13:29 +0300

    Add app routes and app components.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FooterComponent} from './components/footer/footer.component';
+import {HeroComponent} from './components/hero/hero.component';
+import {HeaderComponent} from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent, HeroComponent, HeaderComponent],
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'events',
+    loadComponent: () => import('./components/event-list/event-list.component').then(m => m.EventListComponent),
+  },
+  {
+    path: 'events/:id',
+    loadComponent: () => import('./components/event-detail/event-detail.component').then(m => m.EventDetailComponent),
+  },
+  {
+    path: 'events/:id/participants',
+    loadComponent: () => import('./participants/participant-list/participant-list.component').then(m => m.ParticipantListComponent),
+  },
+  {
+    path: 'participants',
+    loadComponent: () => import('./participants/participant-list/participant-list.component').then(m => m.ParticipantListComponent),
+  },
+  {
+    path: 'participants/new',
+    loadComponent: () => import('./participants/participant-detail/participant-detail.component').then(m => m.ParticipantDetailComponent),
+  },
+  {
+    path: 'participants/:id',
+    loadComponent: () => import('./participants/participant-detail/participant-detail.component').then(m => m.ParticipantDetailComponent),
+  },
+  { path: '', redirectTo: '/events', pathMatch: 'full' },
+  { path: '**', loadComponent: () => import('./components/not-found/not-found.component').then(m => m.NotFoundComponent) },
+];
