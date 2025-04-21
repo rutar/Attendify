0ab3205f8acf55a42483a4888d28ba480b19097e
rutar <rustarg@gmail.com>
2025-04-21 23:58:14 +0300

    Add park-banner component.

---
+++ frontend/src/app/components/park-banner/park-banner.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-park-banner',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './park-banner.component.html',
+  styleUrls: ['./park-banner.component.scss'],
+})
+export class ParkBannerComponent {}
