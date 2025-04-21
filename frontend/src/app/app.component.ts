a9c208e855662b2c654339ae726aec2d723456f6
rutar <rustarg@gmail.com>
2025-04-21 17:04:08 +0300

    Configure frontend application.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,16 +1,38 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {FooterComponent} from './components/footer/footer.component';
-import {HeroComponent} from './components/hero/hero.component';
-import {HeaderComponent} from './components/header/header.component';
+import { Component, computed, signal } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NgIf } from '@angular/common';
+import { PageBannerComponent } from './components/page-banner/page-banner.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FooterComponent, HeroComponent, HeaderComponent],
-  templateUrl: './app.component.html',
   standalone: true,
+  imports: [
+    RouterOutlet,
+    FooterComponent,
+    HeroComponent,
+    HeaderComponent,
+    NgIf,
+    PageBannerComponent
+  ],
+  templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'frontend';
+
+  currentRoute = signal('');
+  showHero = computed(() => this.currentRoute() === '/events');
+  showAddEvent = computed(() => this.currentRoute() === '/events/new');
+  showParticipants = computed(() => /^\/events\/[^/]+\/participants$/.test(this.currentRoute()));
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.currentRoute.set(event.urlAfterRedirects);
+      }
+    });
+  }
 }
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -6,24 +6,28 @@ export const routes: Routes = [
     loadComponent: () => import('./components/event-list/event-list.component').then(m => m.EventListComponent),
   },
   {
+    path: 'events/new',
+    loadComponent: () => import('./components/event-create/event-create.component').then(m => m.EventCreateComponent),
+  },
+  {
     path: 'events/:id',
     loadComponent: () => import('./components/event-detail/event-detail.component').then(m => m.EventDetailComponent),
   },
   {
     path: 'events/:id/participants',
-    loadComponent: () => import('./participants/participant-list/participant-list.component').then(m => m.ParticipantListComponent),
+    loadComponent: () => import('./components/participant-list/participant-list.component').then(m => m.ParticipantListComponent),
   },
   {
     path: 'participants',
-    loadComponent: () => import('./participants/participant-list/participant-list.component').then(m => m.ParticipantListComponent),
+    loadComponent: () => import('./components/participant-list/participant-list.component').then(m => m.ParticipantListComponent),
   },
   {
     path: 'participants/new',
-    loadComponent: () => import('./participants/participant-detail/participant-detail.component').then(m => m.ParticipantDetailComponent),
+    loadComponent: () => import('./components/participant-detail/participant-detail.component').then(m => m.ParticipantDetailComponent),
   },
   {
     path: 'participants/:id',
-    loadComponent: () => import('./participants/participant-detail/participant-detail.component').then(m => m.ParticipantDetailComponent),
+    loadComponent: () => import('./components/participant-detail/participant-detail.component').then(m => m.ParticipantDetailComponent),
   },
   { path: '', redirectTo: '/events', pathMatch: 'full' },
   { path: '**', loadComponent: () => import('./components/not-found/not-found.component').then(m => m.NotFoundComponent) },
