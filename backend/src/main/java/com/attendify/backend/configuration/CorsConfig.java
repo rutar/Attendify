a9b1a0ace26e156595ff95ec69cd163dede9bc66
rutar <rustarg@gmail.com>
2025-05-12 16:21:55 +0300

    Revert "Update CORS configuration to allow Vercel deployment origins."
    This reverts commit a98e445126497eb52ed6e60067dfac0505f3406a.

--- backend/src/main/java/com/attendify/backend/configuration/CorsConfig.java
+++ backend/src/main/java/com/attendify/backend/configuration/CorsConfig.java
@@ -10,9 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:4200",
-                        "http://localhost",
-                        "https://attendify-sooty.vercel.app","http://attendify-sooty.vercel.app")
+                .allowedOrigins("http://localhost:4200", "http://localhost")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
